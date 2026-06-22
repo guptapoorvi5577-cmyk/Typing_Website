b5ba50085721a574a7f929aaee05476374735a99
Poorvi gupta <guptapoorvi5577@gmail.com>
2026-06-22 20:40:06 +0530

    Refactor scoreRoute to update controller imports

--- backend/routes/scoreRoute.js
+++ backend/routes/scoreRoute.js
@@ -1,15 +1,13 @@
 const express = require('express');
-
 const router = express.Router();
 
-const {test, getLatestScore} = require('../controllers/test');
-const {leaderBoard} = require('../controllers/leaderBoard');
-
-const {auth} = require('../middleware/authMiddleware');
+const { test } = require('../controllers/test');
+const { getLatestScore } = require('../controllers/getTestScore');
+const { leaderBoard } = require('../controllers/leaderBoard');
+const { auth } = require('../middleware/authMiddleware');
 
 router.post('/saveTest', auth, test);
 router.get('/latest', auth, getLatestScore);
-
 router.get('/leaderboard', auth, leaderBoard);
 
 module.exports = router;
