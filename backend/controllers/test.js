184b38dfc03ae16d4966da991a6ffb209a24cc23
Poorvi gupta <guptapoorvi5577@gmail.com>
2026-06-22 20:41:09 +0530

    Refactor test function for improved readability

--- backend/controllers/test.js
+++ backend/controllers/test.js
@@ -1,79 +1,22 @@
-//import model
 const Score = require('../models/Score');
 
-exports.test = async(req, res)=>{
+exports.test = async (req, res) => {
+  try {
+    const { wpm, accuracy, duration, paragraphId } = req.body;
+    const userId = req.user.id;
 
-    try{
+    const newResult = { wpm, accuracy, duration, paragraphId };
 
-        //wpm and accuracy
-        const {wpm, accuracy, duration, paragraphId} = req.body;
+    const response = await Score.findOneAndUpdate(
+      { userId },
+      { $push: { scoreHistory: newResult } },
+      { new: true, upsert: true }
+    );
 
-        const userId = req.user.id;
+    res.status(200).json({ success: true, data: response, message: 'score added' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+};
 
-        const newResult = {wpm, accuracy, duration, paragraphId};
-
-        const response = await Score.findOneAndUpdate(
-
-            {userId: userId},
-            {$push: {scoreHistory: newResult}},
-            {new: true, upsert: true}
-
-
-        )
-
-        res.status(200).json({
-            success: true,
-            data: response,
-            message: 'score added'
-        });
-
-    }
-    catch(err){
-
-        console.error(err);
-
-        res.status(500).json({
-            success: false,
-            message: 'Internal server error'
-        })
-
-    }
-    exports.getLatestScore = async(req, res)=>{
-
-    try{
-
-        const userId = req.user.id;
-
-        const scoreDoc = await Score.findOne({ userId });
-
-        if(!scoreDoc || scoreDoc.scoreHistory.length === 0){
-            return res.status(200).json({
-                success: true,
-                data: null,
-                message: 'no scores yet'
-            });
-        }
-
-        const latest = scoreDoc.scoreHistory[scoreDoc.scoreHistory.length - 1];
-
-        res.status(200).json({
-            success: true,
-            data: latest,
-            message: 'fetched'
-        });
-
-    }
-    catch(err){
-
-        console.error(err);
-
-        res.status(500).json({
-            success: false,
-            message: 'internal server error'
-        })
-
-    }
-
-}
-
-}
