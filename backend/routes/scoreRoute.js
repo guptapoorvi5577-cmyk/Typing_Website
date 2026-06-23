8bfa235b58f46ed6d2ce5d9a1a8b822a45f17b78
Poorvi gupta <guptapoorvi5577@gmail.com>
2026-06-24 00:02:07 +0530

    Add route for getting profile statistics

--- backend/routes/scoreRoute.js
+++ backend/routes/scoreRoute.js
@@ -5,9 +5,11 @@ const { test } = require('../controllers/test');
 const { getLatestScore } = require('../controllers/getTestScore');
 const { leaderBoard } = require('../controllers/leaderBoard');
 const { auth } = require('../middleware/authMiddleware');
+const { getProfileStats } = require('../controllers/getProfileStats');
 
 router.post('/saveTest', auth, test);
 router.get('/latest', auth, getLatestScore);
 router.get('/leaderboard', auth, leaderBoard);
+router.get('/stats', auth, getProfileStats);
 
 module.exports = router;
